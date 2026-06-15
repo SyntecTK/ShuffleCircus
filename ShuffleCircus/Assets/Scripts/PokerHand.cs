597896a398ff0bde8d7777b65014f6210f6009cc
Domenik K. <dome@syntectk.com>
2026-06-15 15:24:58 +0200

    Added Score Calculator

---
+++ ShuffleCircus/Assets/Scripts/PokerHand.cs
@@ -0,0 +1,13 @@
+public enum PokerHand
+{
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+    RoyalFlush
+}
---
+++ ShuffleCircus/Assets/Scripts/UI/BoardData.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class BoardData : MonoBehaviour
+{
+    public CardData[,] grid = new CardData[3, 5];
+
+    public void PlaceCard(int row, int column, CardData card)
+    {
+        grid[row, column] = card;
+    }
+
+    public CardData GetCard(int row, int column)
+    {
+        return grid[row, column];
+    }
+
+    public void RemoveCard(int row, int column)
+    {
+        grid[row, column] = null;
+    }
+
+    public bool IsFull()
+    {
+        for(int row = 0; row < grid.GetLength(0); row++)
+        {
+            for(int col = 0; col < grid.GetLength(1); col++)
+            {
+                if (grid[row, col] == null) return false;
+            }
+        }
+        return true;
+    }
+
+
+}
